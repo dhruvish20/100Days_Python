c3308797001f00988fdabd620d2fef01fec51569
dhruvish20 <dhruvish1331@gmail.com>
2024-01-17 22:50:25 +0530

    main

---
+++ day24/main.py
@@ -0,0 +1,45 @@
+from turtle import  Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width = 600 , height = 600)
+screen.bgcolor("black")
+screen.title("My snake game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up , "Up")
+screen.onkey(snake.down , "Down")
+screen.onkey(snake.left , "Left")
+screen.onkey(snake.right , "Right")
+
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.segments[0].distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+        snake.extend()
+
+    if snake.segments[0].xcor() > 280 or snake.segments[0].xcor() < -280 or snake.segments[0].ycor() > 280 or snake.segments[0].ycor() < -280:
+        snake.reset()
+        scoreboard.reset_score()
+
+    for segment in snake.segments[1:]:
+        if snake.segments[0].distance(segment) < 10:
+            snake.reset()
+            scoreboard.reset_score()
+    
+       
+screen.exitonclick()
---
+++ day24/scoreboard.py
@@ -0,0 +1,38 @@
+from turtle import Turtle
+import os
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data.txt")) as data:
+            self.high_score = int(data.read())
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data.txt") , "w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
